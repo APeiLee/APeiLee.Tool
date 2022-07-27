d0507ff5db04447109a0e9dc294dafc7f22dee44
APei <apeilee@qq.com>
2022-07-27 11:41:13 +0800

    新增Md5Helper

---
+++ APeiLee.Tool.Tests/Md5HelperTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using APeiLee.Tool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APeiLee.Tool.Tests
+{
+    public class Md5HelperTests
+    {
+        [Fact()]
+        public void GetMd5Test()
+        {
+            var id = Md5Helper.GetMd5("Hello World!");
+            Assert.Equal("ED076287532E86365E841E92BFC50D8C", id);
+        }
+    }
+}
---
+++ APeiLee.Tool/Md5Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APeiLee.Tool
+{
+    public static class Md5Helper
+    {
+        public static string GetMd5(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return string.Empty;
+            }
+
+            byte[] bytes = Encoding.UTF8.GetBytes(str);
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] hash = md5.ComputeHash(bytes);
+                return BitConverter.ToString(hash).Replace("-", "");
+            }
+        }
+    }
+}
